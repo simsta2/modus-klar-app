389737c03bd02936b15d99f49fd821bc5dd91a11
simsta2 <158079451+simsta2@users.noreply.github.com>
2025-07-31 13:57:56 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>Modus-Klar</h1>
+      <p>App wird gerade eingerichtet...</p>
+    </div>
+  );
+}
+
+export default App;
